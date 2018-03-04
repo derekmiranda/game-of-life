8f50f93445a6ebfc064e850a126f4cf23e084eee
Derek Miranda <derek.miranda.1992@gmail.com>
2018-03-04 00:07:20 -0800

    Switched around axes to make it semantically easier to access points on board

--- script.js
+++ script.js
@@ -3,30 +3,34 @@ const PIXEL_SCALE = 4
 
 const createPoint = (x, y) => ({ x, y })
 const createBoard = ({ width, height, points }) => {
-  const createRowArr = (xVals, y) => {
-    const rowArr = new Array(width).fill(false)
-    xVals.forEach(x => {
-      rowArr[x] = true
+  const createColArr = (yVals, x) => {
+    const colArr = new Array(height).fill(false)
+    yVals.forEach(y => {
+      colArr[y] = true
     })
-    return rowArr
+    return colArr
   }
 
   const boardArr = []
-  for (let curr_y = 0; curr_y <= height; curr_y++) {
-    const xVals = points.filter(pt => pt.y === curr_y).map(pt => pt.x)
-    const rowArr = createRowArr(xVals, curr_y)
-    boardArr.push(rowArr)
+  for (let curr_x = 0; curr_x <= width; curr_x++) {
+    const yVals = points.filter(pt => pt.x === curr_x).map(pt => pt.y)
+    const colArr = createColArr(yVals, curr_x)
+    boardArr.push(colArr)
   }
   return boardArr
+}
+
+function checkPointAgainstNeighbors({ x, y, points }) {
+  // Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+  // Any live cell with two or three live neighbours lives on to the next generation.
+  // Any live cell with more than three live neighbours dies, as if by overpopulation.
+  // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.  
 }
 
 function calcNewBoard(pts) {
   if (!pts) return []
 
-  // Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-  // Any live cell with two or three live neighbours lives on to the next generation.
-  // Any live cell with more than three live neighbours dies, as if by overpopulation.
-  // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
 }
 
 function drawPoint(ctx, x, y, fillStyle = 'navy') {
@@ -35,8 +39,8 @@ function drawPoint(ctx, x, y, fillStyle = 'navy') {
 }
 
 function render(ctx, board) {
-  board.forEach((row, y) => {
-    row.forEach((isAlive, x) => isAlive && drawPoint(ctx, x, y))
+  board.forEach((col, x) => {
+    col.forEach((isAlive, y) => isAlive && drawPoint(ctx, x, y))
   })
 }
 
@@ -58,7 +62,12 @@ function main() {
   const ctx = canvas.getContext('2d')
   const resizedWidth = canvas.width / PIXEL_SCALE 
   const resizedHeight = canvas.height / PIXEL_SCALE 
-  const points = randomPoints(5, resizedWidth, resizedHeight)
+  const middle_x = ~~(resizedWidth / 2)
+  const middle_y = ~~(resizedHeight / 2)
+  const points = [
+    createPoint(middle_x, middle_y),
+    createPoint(middle_x + 1, middle_y),
+  ]
 
   const board = createBoard({
     width: resizedWidth,
